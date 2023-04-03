0652e97d38636565312fa8536135018abb8d96d1
Toma <tomaandrei98@gmail.com>
2023-04-03 12:14:41 +0300

    Inversion of control configuration

---
+++ SpringJourney/src/main/java/org/example/BaseballCoach.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class BaseballCoach implements Coach {
+    @Override
+    public String getDailyWorkout() {
+        return "Spend 30 minutes on batting practice";
+    }
+}
---
+++ SpringJourney/src/main/java/org/example/MyApp.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class MyApp {
+    public static void main(String[] args) {
+        Coach theCoach = new TrackCoach();
+        System.out.println(theCoach.getDailyWorkout());
+    }
+}
