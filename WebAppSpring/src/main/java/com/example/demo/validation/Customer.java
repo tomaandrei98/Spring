44075a1a269487449cdd3e03e8f797735518f866
Toma <tomaandrei98@gmail.com>
2023-04-10 11:36:42 +0300

    Add custom validation annotation

--- WebAppSpring/src/main/java/com/example/demo/validation/Customer.java
+++ WebAppSpring/src/main/java/com/example/demo/validation/Customer.java
@@ -1,5 +1,6 @@
 package com.example.demo.validation;
 
+import com.example.demo.validation.custom.CourseCode;
 import jakarta.validation.constraints.*;
 
 public class Customer {
@@ -9,12 +10,16 @@ public class Customer {
     @Size(min = 2, message = "is required")
     private String lastName;
 
+    @NotNull(message = "is required")
     @Min(value = 0, message = "must be greater than or equal to zero")
     @Max(value = 10, message = "must be less than or equal to 10")
-    private int freePasses;
+    private Integer freePasses;
 
     @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 chars/digits")
     private String postalCode;
+
+    @CourseCode(value = "TOPS", message = "must start with TOPS")
+    private String courseCode;
 
     public String getFirstName() {
         return firstName;
@@ -32,11 +37,11 @@ public class Customer {
         this.lastName = lastName;
     }
 
-    public int getFreePasses() {
+    public Integer getFreePasses() {
         return freePasses;
     }
 
-    public void setFreePasses(int freePasses) {
+    public void setFreePasses(Integer freePasses) {
         this.freePasses = freePasses;
     }
 
@@ -46,5 +51,13 @@ public class Customer {
 
     public void setPostalCode(String postalCode) {
         this.postalCode = postalCode;
+    }
+
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public void setCourseCode(String courseCode) {
+        this.courseCode = courseCode;
     }
 }
