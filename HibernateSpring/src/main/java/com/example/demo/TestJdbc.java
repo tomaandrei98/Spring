932f1b679ba1ce65e141912a59aa87fae3daf028
Toma <tomaandrei98@gmail.com>
2023-04-12 13:18:12 +0300

    Add configuration for one to one relationship

--- HibernateSpring/src/main/java/com/example/demo/TestJdbc.java
+++ HibernateSpring/src/main/java/com/example/demo/TestJdbc.java
@@ -6,7 +6,8 @@ import java.sql.DriverManager;
 public class TestJdbc {
     public static void main(String[] args) {
         try {
-            String jdbcUrl = "jdbc:mysql://localhost:3306/hb_student_tracker";
+//            String jdbcUrl = "jdbc:mysql://localhost:3306/hb_student_tracker";
+            String jdbcUrl = "jdbc:mysql://localhost:3306/hb-01-one-to-one-uni";
             String user = "hbstudent";
             String password = "hbstudent";
 
---
+++ HibernateSpring/src/main/java/com/example/demo/mappings/onetoone/CreateDemo.java
@@ -0,0 +1,38 @@
+package com.example.demo.mappings.onetoone;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+
+public class CreateDemo {
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration()
+                .configure()
+                .addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructorDetail.class)
+                .buildSessionFactory();
+
+        try (factory) {
+            Session session = factory.getCurrentSession();
+
+//            Instructor tempInstructor = new Instructor("Chad", "Darby", "darby@gmail.com");
+//            InstructorDetail tempInstructorDetail =
+//                    new InstructorDetail("http://www.google.com", "love to code");
+//            tempInstructor.setInstructorDetail(tempInstructorDetail);
+
+            Instructor tempInstructor = new Instructor("Toma", "Andrei", "tomaandrei98@gmail.com");
+            InstructorDetail tempInstructorDetail =
+                    new InstructorDetail("http://www.youtube.com", "piano");
+            tempInstructor.setInstructorDetail(tempInstructorDetail);
+
+            session.beginTransaction();
+
+            System.out.println("Saving instructor: " +tempInstructor);
+            session.persist(tempInstructor);
+
+            session.getTransaction().commit();
+            System.out.println("Done");
+        }
+    }
+}
