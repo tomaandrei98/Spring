3c163634ee673fd1402e916ba874daa03437b5f2
Toma <tomaandrei98@gmail.com>
2023-05-13 12:59:42 +0300

    Add custom login form

---
+++ SpringSecurity/src/main/java/com/example/demo/LoginController.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+    @GetMapping("/showMyLoginPage")
+    public String showMyLoginPage() {
+        return "plain-login";
+    }
+}
--- SpringSecurity/src/main/java/com/example/demo/SecurityConfiguration.java
+++ SpringSecurity/src/main/java/com/example/demo/SecurityConfiguration.java
@@ -2,31 +2,33 @@ package com.example.demo;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfiguration {
     @Bean
     public InMemoryUserDetailsManager userDetailsService() {
-        UserDetails user1 = User.withDefaultPasswordEncoder()
-                .username("john")
-                .password("test123")
-                .roles("EMPLOYEE")
-                .build();
-
-        UserDetails user2 = User.withDefaultPasswordEncoder()
-                .username("mary")
-                .password("test123")
-                .roles("MANAGER")
-                .build();
-
-        UserDetails user3 = User.withDefaultPasswordEncoder()
-                .username("susan")
-                .password("test123")
-                .roles("ADMIN")
-                .build();
+        UserDetails user1 = User.withDefaultPasswordEncoder().username("john").password("test123").roles("EMPLOYEE").build();
+        UserDetails user2 = User.withDefaultPasswordEncoder().username("mary").password("test123").roles("MANAGER").build();
+        UserDetails user3 = User.withDefaultPasswordEncoder().username("susan").password("test123").roles("ADMIN").build();
         return new InMemoryUserDetailsManager(user1, user2, user3);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/showMyLoginPage")
+                .loginProcessingUrl("/authenticateTheUser")
+                .permitAll()
+                .and().build();
     }
 }
