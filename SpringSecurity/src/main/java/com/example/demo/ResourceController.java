53edb9056d25accb0528063327e6555e88323d5b
Toma <tomaandrei98@gmail.com>
2023-05-17 11:26:36 +0300

    Display user role

--- SpringSecurity/src/main/java/com/example/demo/ResourceController.java
+++ SpringSecurity/src/main/java/com/example/demo/ResourceController.java
@@ -1,13 +1,17 @@
 package com.example.demo;
 
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class ResourceController {
     @GetMapping("/")
-    public String welcome() {
+    public String welcome(Model model) {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        model.addAttribute("principal", principal);
         return "home";
     }
 }
--- SpringSecurity/src/main/java/com/example/demo/SecurityConfiguration.java
+++ SpringSecurity/src/main/java/com/example/demo/SecurityConfiguration.java
@@ -15,7 +15,7 @@ public class SecurityConfiguration {
     @Bean
     public InMemoryUserDetailsManager userDetailsService() {
         UserDetails user1 = User.withDefaultPasswordEncoder().username("john").password("test123").roles("EMPLOYEE").build();
-        UserDetails user2 = User.withDefaultPasswordEncoder().username("mary").password("test123").roles("MANAGER").build();
+        UserDetails user2 = User.withDefaultPasswordEncoder().username("mary").password("test123").roles("EMPLOYEE", "MANAGER").build();
         UserDetails user3 = User.withDefaultPasswordEncoder().username("susan").password("test123").roles("ADMIN").build();
         return new InMemoryUserDetailsManager(user1, user2, user3);
     }
